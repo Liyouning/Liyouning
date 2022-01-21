bbea167f443b4b0f4cfbd515f454c15e47b71930
宁宁宁宁啊 <98032122+Liyouning@users.noreply.github.com>
2022-01-21 19:16:54 +0800

    用三元操作符判断今天是否为工作日
    数字1-7

--- helloWorld.java
+++ helloWorld.java
@@ -5,26 +5,15 @@ import java.util.Scanner;
 public class helloWorld {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		float a = in.nextFloat();
-		System.out.println("请输入身高："+a+"米");
-		int b = in.nextInt();
-		System.out.println("请输入体重："+b+"kg");
-		float c = b/(a*a);
-		if(c<18.5)
+		int a = in.nextInt();
+		System.out.println("今天是周几？");
+		if(a>7 || a<1)
 		{
-			System.out.println("当前体重过轻！"+"当前的BMI是："+c);
-		}else if(c>=18.5 && c<24) 
+			System.out.println("请输入正确的数字！！！");
+		}else 
 		{
-			System.out.println("当前正常体重！"+"当前的BMI是："+c);
-		}else if(c>=27 && c<30)
-		{
-			System.out.println("当前轻度肥胖！"+"当前的BMI是："+c);
-		}else if(c>=30 && c<35)
-		{
-			System.out.println("当前中度肥胖！"+"当前的BMI是："+c);
-		}else if(c>=35)
-		{
-			System.out.println("当前重度肥胖！"+"当前的BMI是："+c);
+		    String b = a==6|| a==7  ? "今天是周末！":"今天是周"+a+"!"+"今天是工作日！";
+		    System.out.println(b);
 		}
 	}
 
